4c47d40b0adf21f3c7fcb3df8dcfcd13b711af43
PrachiShree-SDE <prachishree111@gmail.com>
2025-08-03 11:59:13 +0530

    Added login  signup buttons to side bar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,9 +1,17 @@
 import { AiFillCloseCircle } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/footer";
+import { useDispatch, useSelector } from "react-redux";
+
 
 function HomeLayout({children}){
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+    const role = useSelector((state) => state?.auth?.role);
 
       function changeWidth() {
         const drawerSide = document.getElementsByClassName('drawer-side');
@@ -18,6 +26,15 @@ function HomeLayout({children}){
         drawerSide[0].style.width = '0';
       }
 
+      function  onLogout(e) {
+        e.preventDefault();
+
+        //todo
+        navigate("/");
+
+      }
+
+
     return(
         <div  className="min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-full">
@@ -28,7 +45,7 @@ function HomeLayout({children}){
                     </label>
                 </div>
                 <div className="drawer-side w-0">
-                         <label htmlFor="my-drawer" className="drawer-overlay">
+                         <label htmlFor="my-drawer" className="drawer-overlay"> </label>
 
                         <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
                             <li className="w-fit absolute right-2 z-50">
@@ -39,6 +56,15 @@ function HomeLayout({children}){
                             <li>
                                 <Link to='/'>Home</Link>
                             </li>
+
+                                {
+                                    isLoggedIn && role === "ADMIN" && (
+                                        <li>
+                                            <Link to="/admin/dashboard">Admin Dashboard</Link>
+                                        </li>
+                                    )
+                                }
+
                              <li>
                                 <Link to='/about'>About us</Link>
                             </li>
@@ -48,9 +74,37 @@ function HomeLayout({children}){
                              <li>
                                 <Link to='/courses'>All courses</Link>
                             </li>
-                        </ul>
 
-                    </label>
+                            {!isLoggedIn?(
+                                <li className="absolute bottom-2 w-[95%] left-2">
+                                    <div className="w-full flex items-center justify-center ">
+                                        <button className=" btn btn-primary px-4 py-1 font-semibold rounded-md w-1/2 ">
+                                            <Link to="/login" >Login</Link>
+                                        </button>
+                                         <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-1/2">
+                                            <Link to="/signup" >Signup</Link>
+                                        </button>
+                                    </div>
+                                </li>
+                            ):(
+
+                                 <li className="absolute bottom-2 w-[95%]">
+                                    <div className="w-full flex items-center justify-center ">
+                                        <button className="btn btn-primary px-4 py-1 font-semibold rounded-md w-1/2">
+                                            <Link to="/user/profile">Profile</Link>
+                                        </button>
+                                         <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-1/2">
+                                            <Link onClick={onLogout} >Logout</Link>
+                                        </button>
+                                    </div>
+                                </li>
+
+
+                            )
+
+                            }
+
+                        </ul>
                 </div>
             </div>
 
